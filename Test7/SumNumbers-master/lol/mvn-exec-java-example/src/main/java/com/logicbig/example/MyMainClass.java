e9a73950c37995b90e045dddd31d5dd382eb8407
= <=>
2020-01-20 21:37:28 +0300

    SumNumbers-master

--- Test7/SumNumbers-master/lol/mvn-exec-java-example/src/main/java/com/logicbig/example/MyMainClass.java
+++
@@ -1,9 +0,0 @@
-package com.logicbig.example;
-
-import java.util.Arrays;
-
-public class MyMainClass {
-    public static void main(String[] args) {
-        Arrays.stream(args).forEach(System.out::println);
-    }
-}
