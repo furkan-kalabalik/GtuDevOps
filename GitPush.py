69414b8f1615a412764cad60e4d0c8ab3302acfe
= <=>
2020-01-21 12:17:24 +0300

    Group1 - Build Success

--- GitPush.py
+++ GitPush.py
@@ -5,18 +5,22 @@ import time
 # git commit
 def commit():
     # If it is a directory, copies it to repository and deletes from current directory.
-    os.popen('git add .')
+    os.popen('git  add .')
+    #os.popen('git -C ' + filename + '  add .')
     time.sleep(1)
-    os.popen('git commit -m \" Group1 - Build Success \"')
+    os.popen('git  commit -m \" Group1 - Build Success \"')
+    #os.popen('git -C ' + filename + '  commit -m \" Group1 - Build Success \"')
 
 # git push
 def push(id,password,url):
     # Finds project name by parsing the incoming url.
     # Updates remote repository url by using the incoming github id and password. 
     os.popen('git  remote set-url origin https://'+id+':'+password+'@github.com/'+id+'/GtuDevOps'+'.git')
+    #os.popen('git  -C ' + filename + '  remote set-url origin https://'+id+':'+password+'@github.com/'+id+'/GtuDevOps'+'.git')
     time.sleep(1)
     # Performs push
-    os.popen('git push -u origin master')
+    #os.popen('git -C ' + filename + '  push -f origin master')
+    os.popen('git  push -f origin master')
 
 
 
